8c9148282e83115bb790e9319a5b362f57a7dc0c
Abel Pereira <abelix897@gmail.com>
2022-10-30 18:35:05 +0000

    tester working now

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -12,14 +12,14 @@
 
 #include "libft.h"
 
-char	*ft_strtrim(char const *s1, char const *set)
-{
-	size_t	i;
+// char	*ft_strtrim(char const *s1, char const *set)
+// {
+// 	size_t	i;
 
-	i = 0;
-	if (!s1 || !set)
-		return (0);
-	while (s1[i] && ft_strchr(set, s1[i]))
-		i++;
-	return (ft_substr(s1[i], 0, ft_strrchr(set, s1[ft_strlen(s1)])));
-}
+// 	i = 0;
+// 	if (!s1 || !set)
+// 		return (0);
+// 	while (s1[i] && ft_strchr(set, s1[i]))
+// 		i++;
+// 	return (ft_substr(s1[i], 0, ft_strrchr(set, s1[ft_strlen(s1)])));
+// }
