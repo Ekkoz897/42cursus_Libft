59349303007f59a58720df338ac3223dc5c36b86
Abel Tiago Pereira <apereira@c1r16s3.42lisboa.com>
2022-11-04 13:39:04 +0000

    pulling to 42 school pc

--- README.md
+++
@@ -1,9 +0,0 @@
-# 42_Piscine_Ausgut_2022
-
-Hey everyone, this is some of the work I did when I participated in the piscine at 42 school in Lisbon.
-
-I hope it helps you out !
-
-Keep Swimming !
-
-#RTFM
---
+++ libft/ft_strmapi.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apereira <apereira@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/02 16:24:48 by apereira          #+#    #+#             */
+/*   Updated: 2022/11/02 17:08:43 by apereira         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	unsigned int	i;
+	char			*str;
+
+	if (!s || !f)
+		return (NULL);
+	str = malloc(sizeof(char) * ft_strlen(s) + 1);
+	if (!str)
+		return (NULL);
+	while (s[i])
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
