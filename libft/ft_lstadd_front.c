8ea2f2e13472d28b7e7ebcc94dfa042eb0e8d1c5
Ekkoz897 <abelix897@gmail.com>
2022-11-12 15:34:45 +0000

    ahd

--- libft/ft_lstadd_front.c
+++ libft/ft_lstadd_front.c
@@ -12,6 +12,13 @@
 
 #include "libft.h"
 
+/**
+ * Adds a new element to the beginning of the list.
+ * 
+ * @param lst A pointer to a pointer to a linked list.
+ * @param new the new element to add to the list
+ */
+
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
 	new->next = *lst;
